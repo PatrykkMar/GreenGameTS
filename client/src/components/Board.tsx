30d6fd7c396b80975f4bfe18edf9ac5ea39cc984
PatrykMargasinski <49985857+PatrykMargasinski@users.noreply.github.com>
2025-12-06 14:59:36 +0100

    Board template improvement

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -8,21 +8,39 @@ export default function Board() {
 
 
   return (
-    <div className="container text-center">
     <div className="container-box">
-        <input type="text" className="input-box top" placeholder="Tekst"/>
-        <input type="text" className="input-box bottom" placeholder="Tekst"/>
-        <input type="text" className="input-box left" placeholder="Tekst"/>
-        <input type="text" className="input-box right" placeholder="Tekst"/>
+        <input className="input-box top" placeholder="Tekst" />
+        <input className="input-box bottom" placeholder="Tekst" />
+        <input className="input-box left" placeholder="Tekst" />
+        <input className="input-box right" placeholder="Tekst" />
 
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-    </div>
-    <div>
-        <button className="btn btn-success ready-btn">READY</button>
-    </div>
+        <div className="tile">
+            <label className="tile-label top">G1</label>
+            <label className="tile-label bottom">D1</label>
+            <label className="tile-label left">L1</label>
+            <label className="tile-label right">P1</label>
+        </div>
+
+        <div className="tile">
+            <label className="tile-label top">G2</label>
+            <label className="tile-label bottom">D2</label>
+            <label className="tile-label left">L2</label>
+            <label className="tile-label right">P2</label>
+        </div>
+
+        <div className="tile">
+            <label className="tile-label top">G3</label>
+            <label className="tile-label bottom">D3</label>
+            <label className="tile-label left">L3</label>
+            <label className="tile-label right">P3</label>
+        </div>
+
+        <div className="tile">
+            <label className="tile-label top">G4</label>
+            <label className="tile-label bottom">D4</label>
+            <label className="tile-label left">L4</label>
+            <label className="tile-label right">P4</label>
+        </div>
     </div>
   );
 }
