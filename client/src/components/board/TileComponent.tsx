23cf0c728107522c23a3dc0a40181ee91f2eccbe
PatrykMargasinski <49985857+PatrykMargasinski@users.noreply.github.com>
2025-12-26 18:57:46 +0100

    Better css

--- client/src/components/board/TileComponent.tsx
+++ client/src/components/board/TileComponent.tsx
@@ -1,5 +1,5 @@
 import type BoardTile from "@shared/models/BoardTile";
-import  { useState } from "react";
+import { useState } from "react";
 
 interface TileProps {
   boardTile: BoardTile;
@@ -9,22 +9,21 @@ export default function TileComponent({ boardTile }: TileProps) {
   const [rotation, setRotation] = useState(0);
 
   const handleRotate = () => {
-    setRotation((prev) => prev + 90); 
+    setRotation(prev => prev + 90);
   };
 
   return (
     <div className="tile">
       <label className="tile-label top">{boardTile.words[0]}</label>
-      <label className="tile-label bottom">{boardTile.words[1]}</label>
-      <label className="tile-label left">{boardTile.words[2]}</label>
-      <label className="tile-label right">{boardTile.words[3]}</label>
+      <label className="tile-label right">{boardTile.words[1]}</label>
+      <label className="tile-label bottom">{boardTile.words[2]}</label>
+      <label className="tile-label left">{boardTile.words[3]}</label>
 
       <button
         className="rotate-btn"
         onClick={handleRotate}
         style={{ transform: `translate(-50%, -50%) rotate(${rotation}deg)` }}
-      >
-        Rotate
+      >↻
       </button>
     </div>
   );
